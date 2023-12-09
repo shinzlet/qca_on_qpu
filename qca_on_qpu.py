a80d38a4ce8fe45b8230b033e6908aa1516c202f
Seth Hinz <sethhinz@me.com>
2023-12-08 21:15:25 -0800

    switched from bigham to qubo

--- qca_on_qpu.py
+++ qca_on_qpu.py
@@ -1,7 +1,22 @@
 import argparse
+from bigham import extract_polarization
 from qcadtrans import QCACircuit
 
-from bigham import *
+import dwave
+import dwave.embedding
+import dwave.inspector
+from dwave.system import DWaveSampler, EmbeddingComposite
+from dwave.cloud import Client
+from dwave.cloud.exceptions import SolverNotFoundError
+import dimod
+from dimod.reference.samplers import ExactSolver
+from minorminer import find_embedding
+import neal
+
+# general math and Python dependencies
+import math
+import numpy as np
+import itertools
 
 parser = argparse.ArgumentParser(
                     prog='qca_on_qpu',
@@ -18,6 +33,9 @@ NORMAL_T = 0
 INPUT_T  = 1
 OUTPUT_T = 2
 FIXED_T  = 3
+
+ADJACENT_DIRECTIONS = np.array([[-1, 0], [0, 1], [1, 0], [0, -1]])
+DIAGONAL_DIRECTIONS = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
 
 # The kink energy for adjacent cells (i.e. cells with a center to center distance
 # of 1 unit)
@@ -60,15 +78,117 @@ for i in range(len(circuit.nodes)):
     elif cf == FIXED_T:
         drivers[pos] = float(node["pol"])
 
-num_input_states = 2 ** len(inputs)
-for state in range(num_input_states):
-    all_drivers = drivers
+# TODO: iterate over all input states
+input_state = 0
+all_drivers = drivers
 
-    for input_idx, (name, pos) in enumerate(inputs.items()):
-        all_drivers[pos] = extract_polarization(state, input_idx)
+for input_idx, (name, pos) in enumerate(inputs.items()):
+    all_drivers[pos] = extract_polarization(input_state, input_idx)
 
-    # print(full_hamiltonian(cells, all_drivers, Ek0, t))
-    result = simulate_circuit(cells, all_drivers, Ek0, t)
-    plot_circuit({**result, **all_drivers}, all_drivers, f"Drivers: {all_drivers}", f"images/{state}.png")
-    # break
-# def full_hamiltonian(circuit, drivers, Ek0, t):
+# Using ICHA, and only considering direct neighbours (diagonals included)
+linear = {}
+quadratic = {}
+
+def sum_neighbours(pos, directions, cb):
+    pos = np.array(pos)
+    total = 0
+    for i in range(directions.shape[0]):
+        total += cb(pos, pos + directions[i, :])
+    return total
+
+def driver_contribution(pos, other_pos):
+    r = np.linalg.norm(pos - other_pos)
+    other_pos_tuple = (other_pos[0], other_pos[1])
+    if other_pos_tuple in all_drivers:
+        return all_drivers[other_pos_tuple] / r ** 5
+    return 0
+
+def scale_func(f, multiplier):
+   return lambda *args, **kwargs: -f(*args, **kwargs)
+
+cell_order = list(cells)
+for (i, pos_i) in enumerate(cells):
+    # The linear term includes the effect of drivers on this cell.
+    linear[pos_i] = 0
+    linear[pos_i] += sum_neighbours(pos_i, ADJACENT_DIRECTIONS, scale_func(driver_contribution, -Ek0))
+    linear[pos_i] += sum_neighbours(pos_i, DIAGONAL_DIRECTIONS, scale_func(driver_contribution, Ek0))
+
+    # Cells that are adjacent to this one should have a negative energy
+    # contribution when the quadratic term is positive (i.e. they are of
+    # the same sign) and a positive sign when the quadratic term is negative.
+
+    # The quadratic term includes the effect of adjacent normal cells.
+    for j in range(i+1, len(cells)):
+        pos_j = cell_order[j]
+        # If cells i and j are adjacent, 
+        r = np.linalg.norm(np.array(pos_i) - np.array(pos_j))
+        if r > 0.99 and r < 1.01:
+            # adjacent (negative energy terms means they should be the same signs)
+            quadratic[(pos_i, pos_j)] = -Ek0
+        elif r > 1.4 and r < 1.42:
+            # diagonal (positive energy term means they should be opposite signs)
+            quadratic[(pos_i, pos_j)] = Ek0 / r ** 5
+
+# print(all_drivers)
+# print(list(cells))
+# print(linear)
+# print(quadratic)
+# exit()
+
+# # create edgelist (note that {} initializes Python dicts)
+# linear = {}         # qubit self-bias
+# quadratic = {}      # inter-qubit bias
+# for i in range(N):
+#     linear[i] = h[i]
+#     for j in range(i+1, N):
+#         if J[i][j] != 0:
+#             quadratic[(i,j)] = J[i][j]
+# 
+use_classical = True
+qpu_arch = 'pegasus'
+
+# construct a bqm containing the provided self-biases (linear) and couplings
+# (quadratic). Specify the problem as SPIN (Ising).
+print('Constructing BQM...')
+bqm = dimod.BinaryQuadraticModel(linear, quadratic, 0, dimod.SPIN)
+
+# get DWave sampler and target mapping edgelist
+print('Choosing solver...')
+client = Client.from_config()
+solver = None
+qca_arch = 'pegasus'
+try:
+    if qpu_arch == 'zephyr':
+        solver = client.get_solver('Advantage2_prototype1.1').id
+    elif qpu_arch == 'pegasus':
+        solver = client.get_solver('Advantage_system4.1').id
+    elif qpu_arch == 'chimera':
+        solver = client.get_solver('DW_2000Q_6').id
+    else:
+        raise ValueError('Specified QPU architecture is not supported.')
+except SolverNotFoundError:
+    print(f'The pre-programmed D-Wave solver name for architecture '
+            '\'{qpu_arch}\' is not available. Find the latest available '
+            'solvers by:\n'
+            'from dwave.cloud import Client\nclient = Client.from_config()\n'
+            'client.get_solvers()\nAnd update this script.')
+    raise
+
+# get the specified QPU
+dwave_sampler = DWaveSampler(solver=solver)
+
+# run the problem
+use_result = []
+sampler = None
+response = None
+if use_classical:
+    print('Choosing classical sampler...')
+    sampler = neal.SimulatedAnnealingSampler()
+else:
+    print('Choosing D-Wave QPU as sampler...')
+    sampler = EmbeddingComposite(dwave_sampler)
+
+response = sampler.sample(bqm, num_reads=50)
+print('Problem completed from selected sampler.')
+
+print(response.record)
